0d7a3737c289ecb454d96fde47bbfd7795eb052e
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2019-12-30 01:42:26 +0100

    add async version of get_external_resource

--- octobot_commons/external_resources_manager.py
+++ octobot_commons/external_resources_manager.py
@@ -21,15 +21,27 @@ from octobot_commons.logging.logging_util import get_logger
 from octobot_commons.constants import EXTERNAL_RESOURCE_URL
 
 
+def _handle_exception(exception, resource_key, catch_exception, default_response):
+    if catch_exception:
+        get_logger("ExternalResourcesManager") \
+            .error(f"Exception when calling get_external_resource for {resource_key} key: {exception}")
+        return default_response
+    else:
+        raise exception
+
+
 def get_external_resource(resource_key, catch_exception=False, default_response=""):
     try:
-        external_resource_url = EXTERNAL_RESOURCE_URL
-        external_resources = json.loads(requests.get(external_resource_url).text)
+        external_resources = json.loads(requests.get(EXTERNAL_RESOURCE_URL).text)
         return external_resources[resource_key]
     except Exception as e:
-        if catch_exception:
-            get_logger("ExternalResourcesManager")\
-                .error(f"Exception when calling get_external_resource for {resource_key} key: {e}")
-            return default_response
-        else:
-            raise e
+        return _handle_exception(e, resource_key, catch_exception, default_response)
+
+
+async def async_get_external_resource(resource_key, aiohttp_session, catch_exception=False, default_response=""):
+    try:
+        async with aiohttp_session.get(EXTERNAL_RESOURCE_URL) as resp:
+            external_resources = json.loads(resp.text())
+            return external_resources[resource_key]
+    except Exception as e:
+        return _handle_exception(e, resource_key, catch_exception, default_response)
