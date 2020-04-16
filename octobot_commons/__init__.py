12410eb1f5b2659b2596cf3df1aecd8e09768016
Herklos <herklos.dev@protonmail.com>
2020-04-16 23:38:06 +0200

    [Version] v1.3.10

--- octobot_commons/__init__.py
+++ octobot_commons/__init__.py
@@ -15,7 +15,7 @@
 #  License along with this library.
 
 PROJECT_NAME = "OctoBot-Commons"
-VERSION = "1.3.9"  # major.minor.revision
+VERSION = "1.3.10"  # major.minor.revision
 
 MARKET_SEPARATOR = "/"
 DICT_BULLET_TOKEN_STR = "\n "
