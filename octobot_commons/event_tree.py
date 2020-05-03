9eeebfce96c88549b1f11176c92c2061bfa293b9
Herklos <herklos.dev@protonmail.com>
2020-05-03 09:42:59 +0200

    [Version] v1.3.15

--- octobot_commons/event_tree.py
+++ octobot_commons/event_tree.py
@@ -14,8 +14,6 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
-import asyncio
-from asyncio import ALL_COMPLETED, Event
 
 
 class EventTreeNode:
