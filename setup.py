6441041dc607473d9b9c4677dbcc7b9bca4e31ac
Herklos <herklos.dev@protonmail.com>
2020-10-23 13:47:50 +0200

    [Compatibility] Python 3.8

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ with open('README.md', encoding='utf-8') as f:
     DESCRIPTION = f.read()
 
 REQUIRED = open('requirements.txt').readlines()
-REQUIRES_PYTHON = '>=3.7'
+REQUIRES_PYTHON = '>=3.8'
 CYTHON_DEBUG = False if not os.getenv('CYTHON_DEBUG') else os.getenv('CYTHON_DEBUG')
 
 setup(
@@ -92,6 +92,6 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 )
