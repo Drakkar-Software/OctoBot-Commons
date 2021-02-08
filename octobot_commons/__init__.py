b43de5da4e254ef580b6cc977518b15daf5821cd
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-02-08 08:19:46 +0100

    [Version] v1.5.4

--- octobot_commons/__init__.py
+++ octobot_commons/__init__.py
@@ -16,7 +16,7 @@
 #  License along with this library.
 
 PROJECT_NAME = "OctoBot-Commons"
-VERSION = "1.5.3"  # major.minor.revision
+VERSION = "1.5.4"  # major.minor.revision
 
 MARKET_SEPARATOR = "/"
 DICT_BULLET_TOKEN_STR = "\n "
