cde2129b609ff618e237d31d62bc47382ac6c77f
herklos <herklos.dev@protonmail.com>
2021-04-01 18:44:08 +0200

    [Version] 1.5.16

--- octobot_commons/aiohttp_util.py
+++ octobot_commons/aiohttp_util.py
@@ -14,11 +14,14 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 
-async def download_stream_file(output_file,
-                               file_url,
-                               aiohttp_session,
-                               data_chunk_size=5*2**20,
-                               is_aiofiles_output_file=False):
+
+async def download_stream_file(
+    output_file,
+    file_url,
+    aiohttp_session,
+    data_chunk_size=5 * 2 ** 20,
+    is_aiofiles_output_file=False,
+):
     """
     Download a big file with an aiohttp session
     :param output_file: the output file
@@ -29,7 +32,9 @@ async def download_stream_file(output_file,
     """
     async with aiohttp_session.get(file_url) as resp:
         if resp.status != 200:
-            raise RuntimeError(f"Failed to download file at url : {file_url} (status: {resp.status})")
+            raise RuntimeError(
+                f"Failed to download file at url : {file_url} (status: {resp.status})"
+            )
         while True:
             chunk = await resp.content.read(data_chunk_size)
             if not chunk:
