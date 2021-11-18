e7e996d6bceb43ad4063366c3e0356abc5f61155
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2021-11-18 23:45:20 +0100

    [Cache] cache version 1

--- octobot_commons/databases/bases/cache_database.py
+++ octobot_commons/databases/bases/cache_database.py
@@ -13,8 +13,53 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
+import octobot_commons.enums as enums
 import octobot_commons.databases.bases.base_database as base_database
+import octobot_commons.databases.adaptors as adaptors
 
 
 class CacheDatabase(base_database.BaseDatabase):
-    CACHE_TABLE = "cache"
+    CACHE_TABLE = enums.CacheDatabaseTables.CACHE.value
+    CACHE_METADATA_TABLE = enums.CacheDatabaseTables.METADATA.value
+
+    def __init__(self, file_path: str, database_adaptor=adaptors.TinyDBAdaptor, cache_size=None, **kwargs):
+        super().__init__(file_path, database_adaptor=database_adaptor, cache_size=cache_size, **kwargs)
+        self._are_metadata_written = False
+        self._local_cache = None
+
+    def generate_metadata(self) -> dict:
+        """
+        Override if more metadata are required
+        :return: the metadata dict
+        """
+        return {
+            enums.CacheDatabaseColumns.TYPE.value: self.__class__.__name__,
+        }
+
+    async def _ensure_metadata(self):
+        if not self._are_metadata_written:
+            await self._database.upsert(self.CACHE_METADATA_TABLE, self.generate_metadata(), None, uuid=1)
+            self._are_metadata_written = True
+
+    async def _ensure_local_cache(self, identifier_key, update=False):
+        if update or self._local_cache is None:
+            self._local_cache = {
+                cache[identifier_key]: cache
+                for cache in await self.get_cache()
+            }
+
+    async def get_metadata(self):
+        return await self._database.select(self.CACHE_METADATA_TABLE, None, uuid=1)
+
+    async def get_cache(self):
+        return await self._database.select(self.CACHE_TABLE, None)
+
+    async def _get_from_local_cache(self, identifier_key, identifier_value, sub_key):
+        await self._ensure_local_cache(identifier_key)
+        return self._local_cache[identifier_value][sub_key]
+
+    async def _needs_update(self, identifier_key, identifier_value, sub_key, value) -> bool:
+        try:
+            return await self._get_from_local_cache(identifier_key, identifier_value, sub_key) != value
+        except KeyError:
+            return True
