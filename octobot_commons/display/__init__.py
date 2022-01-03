12c93b3143c1fc3e3ca88d2f31004f1a7b80e72d
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-01-03 13:02:23 +0100

    [DisplayTranslator] add DisplayTranslator

---
+++ octobot_commons/display/__init__.py
@@ -0,0 +1,31 @@
+#  Drakkar-Software OctoBot-Commons
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+
+from octobot_commons.display import display_translator
+from octobot_commons.display.display_translator import (
+    DisplayTranslator,
+)
+
+from octobot_commons.display import display_factory
+from octobot_commons.display.display_factory import (
+    display_translator_factory,
+)
+
+
+__all__ = [
+    "DisplayTranslator",
+    "display_translator_factory"
+]
