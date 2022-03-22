8f78fc9befd8f3e3209e7bd7611be3abe7b49f0e
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-03-22 12:29:48 +0000

    [Tentacles] remove TentaclesRequirements

--- octobot_commons/tentacles_management/__init__.py
+++ octobot_commons/tentacles_management/__init__.py
@@ -16,7 +16,6 @@
 
 from octobot_commons.tentacles_management import abstract_tentacle
 from octobot_commons.tentacles_management import class_inspector
-from octobot_commons.tentacles_management import tentacle_requirements
 
 from octobot_commons.tentacles_management.abstract_tentacle import AbstractTentacle
 from octobot_commons.tentacles_management.class_inspector import (
@@ -31,7 +30,6 @@ from octobot_commons.tentacles_management.class_inspector import (
     get_all_classes_from_parent,
     get_single_deepest_child_class,
 )
-from octobot_commons.tentacles_management.tentacle_requirements import TentacleRequirements
 
 __all__ = [
     "AbstractTentacle",
@@ -45,5 +43,4 @@ __all__ = [
     "is_abstract_using_inspection_and_class_naming",
     "get_all_classes_from_parent",
     "get_single_deepest_child_class",
-    "TentacleRequirements",
 ]
