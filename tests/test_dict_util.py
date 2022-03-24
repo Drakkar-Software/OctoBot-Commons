859d3a0bb17c9b4f060dafe1e84885390ac9885b
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-03-24 11:06:27 +0000

    [Cache] improve readability and extract contains_each_element

--- tests/test_dict_util.py
+++ tests/test_dict_util.py
@@ -13,15 +13,15 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
-from octobot_commons.dict_util import find_nested_value, check_and_merge_values_from_reference
+import octobot_commons.dict_util as dict_util
 
 
 def test_find_nested_value():
-    assert find_nested_value({"a": 1, "b": 2}, "b") == (True, 2)
-    assert find_nested_value({"a": 1, "b": 2}, "c") == (False, "c")
-    assert find_nested_value({"a": 1, "b": {"c": 5, "d": "e"}}, "d") == (True, "e")
-    assert find_nested_value({"a": 1, "b": {"c": 5, "d": {"f": [1, 2, 3]}}}, "f") == (True, [1, 2, 3])
-    assert find_nested_value({"a": {"e": 7}, "b": {"c": {"t": 5}, "d": {"f": [1, 2, 3], "y": 1}}}, "y") == (True, 1)
+    assert dict_util.find_nested_value({"a": 1, "b": 2}, "b") == (True, 2)
+    assert dict_util.find_nested_value({"a": 1, "b": 2}, "c") == (False, "c")
+    assert dict_util.find_nested_value({"a": 1, "b": {"c": 5, "d": "e"}}, "d") == (True, "e")
+    assert dict_util.find_nested_value({"a": 1, "b": {"c": 5, "d": {"f": [1, 2, 3]}}}, "f") == (True, [1, 2, 3])
+    assert dict_util.find_nested_value({"a": {"e": 7}, "b": {"c": {"t": 5}, "d": {"f": [1, 2, 3], "y": 1}}}, "y") == (True, 1)
     complex_dict = {"a": {"e": 7}, "b": {"c": {"t": [{
         "ab": [8, 9, 10],
         "cd": {
@@ -37,9 +37,9 @@ def test_find_nested_value():
             "up": "pa",
             "123": 1234
         }]}, "d": {"f": [1, 2, 3], "y": 1}}}
-    assert find_nested_value(complex_dict, "abc") == (True, "def")
-    assert find_nested_value(complex_dict, "5") == (True, 5)
-    assert find_nested_value(complex_dict, "123") == (True, 1234)
+    assert dict_util.find_nested_value(complex_dict, "abc") == (True, "def")
+    assert dict_util.find_nested_value(complex_dict, "5") == (True, 5)
+    assert dict_util.find_nested_value(complex_dict, "123") == (True, 1234)
 
 
 def test_check_and_merge_values_from_reference():
@@ -54,10 +54,41 @@ def test_check_and_merge_values_from_reference():
         "d": 4
     }
     exception_list = ["d"]
-    check_and_merge_values_from_reference(current_dict, ref_dict, exception_list)
+    dict_util.check_and_merge_values_from_reference(current_dict, ref_dict, exception_list)
     assert current_dict == {
         "a": 1,
         "b": 5,
         "c": 3,
         "d": 1
     }
+
+
+def test_contains_each_element():
+    assert dict_util.contains_each_element(
+        {},
+        {}
+    ) is True
+    assert dict_util.contains_each_element(
+        {},
+        {"1": 1}
+    ) is False
+    assert dict_util.contains_each_element(
+        {"1": 1},
+        {"1": 1}
+    ) is True
+    assert dict_util.contains_each_element(
+        {"1": 1, "2": 2},
+        {"1": 1}
+    ) is True
+    assert dict_util.contains_each_element(
+        {"1": 1},
+        {"1": 1, "2": 2}
+    ) is False
+    assert dict_util.contains_each_element(
+        {"1": 1},
+        {}
+    ) is True
+    assert dict_util.contains_each_element(
+        {"1": 2},
+        {"1": 1}
+    ) is False
