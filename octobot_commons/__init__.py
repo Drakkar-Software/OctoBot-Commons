9ec3360aa63da76db89cc266bbe2b85c2a125a20
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2022-11-23 09:56:41 +0100

    [Version] v1.7.32

--- octobot_commons/__init__.py
+++ octobot_commons/__init__.py
@@ -16,7 +16,7 @@
 #  License along with this library.
 
 PROJECT_NAME = "OctoBot-Commons"
-VERSION = "1.7.31"  # major.minor.revision
+VERSION = "1.7.32"  # major.minor.revision
 
 MARKET_SEPARATOR = "/"
 SETTLEMENT_ASSET_SEPARATOR = ":"
