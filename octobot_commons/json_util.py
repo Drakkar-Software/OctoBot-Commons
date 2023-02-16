52bae73b9107a421c7f6ba5be9677457c9aa8eef
Guillaume De Saint Martin <guillaume.minette_de_saintmartin@insa-rouen.fr>
2023-02-16 13:10:33 +0100

    [JSON] handle common json errors when reading file

--- octobot_commons/json_util.py
+++ octobot_commons/json_util.py
@@ -15,6 +15,10 @@
 #  License along with this library.
 import json
 import jsonschema
+import octobot_commons.logging
+
+
+LOGGER_NAME = "json_util"
 
 
 def validate(config, schema_file) -> None:
@@ -27,3 +31,32 @@ def validate(config, schema_file) -> None:
     with open(schema_file) as json_schema:
         loaded_schema = json.load(json_schema)
     jsonschema.validate(instance=config, schema=loaded_schema)
+
+
+def read_file(file_path: str, raise_errors: bool = True, on_error_value: dict = None, open_mode="r") -> dict:
+    """
+    Read a load the given file with json.load()
+    :param file_path: file to read
+    :param raise_errors: when True will forward errors. Will just log errors otherwise
+    :param on_error_value: return this value when raise_errors is False and an error occurs
+    :param open_mode: the file open mode to give to open()
+    :return: the parsed file or default value on error if possible
+    """
+    try:
+        with open(file_path, open_mode) as open_file:
+            return json.load(open_file)
+    except PermissionError as err:
+        if raise_errors:
+            raise
+        octobot_commons.logging.get_logger(LOGGER_NAME).error(
+            f"Permission error when reading {file_path} file: {err}."
+        )
+    except Exception as err:
+        if raise_errors:
+            raise
+        octobot_commons.logging.get_logger(LOGGER_NAME).exception(
+            f"Unexpected error when reading {file_path} file: {err}."
+        )
+    if on_error_value is None:
+        raise ValueError("on_error_value is unset")
+    return on_error_value
