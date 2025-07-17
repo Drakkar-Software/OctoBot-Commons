967c98aa85521d3a444a463ab57264bf19da8776
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2025-07-17 10:37:21 +0200

    [Time] fix timezone deprecation

--- tests/test_timestamp_util.py
+++ tests/test_timestamp_util.py
@@ -14,6 +14,8 @@
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
 import time
+import mock
+from datetime import timezone, timedelta
 
 from octobot_commons.timestamp_util import is_valid_timestamp, get_now_time, datetime_to_timestamp, \
     convert_timestamp_to_datetime
@@ -25,6 +27,42 @@ def test_is_valid_timestamp():
 
 
 def test_datetime_to_timestamp():
-    assert datetime_to_timestamp(convert_timestamp_to_datetime(322548600, time_format="%d/%m/%y %H:%M"),
-                                 date_time_format="%d/%m/%y %H:%M") == 322548600
+    date_str = convert_timestamp_to_datetime(1737331200, time_format="%d/%m/%y %H:%M", local_timezone=True)
+    assert datetime_to_timestamp(date_str, "%d/%m/%y %H:%M") == 1737331200
 
+
+def test_convert_timestamp_to_datetime_default():
+    # 1 Jan 2020 00:00 UTC
+    ts = 1577836800
+    assert convert_timestamp_to_datetime(ts) == "01/01/20 00:00"
+
+
+def test_convert_timestamp_to_datetime_custom_format():
+    ts = 1577836800
+    assert convert_timestamp_to_datetime(ts, time_format="%Y-%m-%d") == "2020-01-01"
+
+
+def test_convert_timestamp_to_datetime_local_timezone(monkeypatch):
+    # Patch LOCAL_TIMEZONE to UTC+2 for test
+    dummy_tz = timezone(timedelta(hours=2))
+    with mock.patch("octobot_commons.timestamp_util.LOCAL_TIMEZONE", dummy_tz):
+        ts = 1577836800  # 1 Jan 2020 00:00 UTC
+        # Should be 02:00 in UTC+2
+        assert convert_timestamp_to_datetime(ts, local_timezone=True) == "01/01/20 02:00"
+
+
+def test_convert_timestamp_to_datetime_epoch():
+    assert convert_timestamp_to_datetime(0) == "01/01/70 00:00"
+
+
+def test_convert_timestamp_to_datetime_far_future():
+    ts = 32503680000  # year 3000
+    assert convert_timestamp_to_datetime(ts, time_format="%Y") == "3000"
+
+
+def test_convert_timestamp_to_datetime_negative():
+    # Negative timestamp: before epoch
+    ts = -1
+    result = convert_timestamp_to_datetime(ts)
+    # Should not raise, but result is system-dependent
+    assert isinstance(result, str)
