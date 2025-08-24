704a69643a72f9bcc62e851f84a04720992ec015
Guillaume De Saint Martin <guillaumemdsm@gmail.com>
2025-08-24 17:21:33 +0200

    [Version] v1.9.82

--- octobot_commons/__init__.py
+++ octobot_commons/__init__.py
@@ -16,7 +16,7 @@
 #  License along with this library.
 
 PROJECT_NAME = "OctoBot-Commons"
-VERSION = "1.9.81"  # major.minor.revision
+VERSION = "1.9.82"  # major.minor.revision
 
 MARKET_SEPARATOR = "/"
 SETTLEMENT_ASSET_SEPARATOR = ":"
