1ed3af84606c0d4a6203df7a4af5f64f07890fa7
Herklos <herklos@drakkar.software>
2026-01-03 23:43:46 +0100

    Add CONFIG_EXCHANGE_OPTION constant

--- octobot_commons/__init__.py
+++ octobot_commons/__init__.py
@@ -16,7 +16,7 @@
 #  License along with this library.
 
 PROJECT_NAME = "OctoBot-Commons"
-VERSION = "1.10.0"  # major.minor.revision
+VERSION = "1.10.1"  # major.minor.revision
 
 MARKET_SEPARATOR = "/"
 SETTLEMENT_ASSET_SEPARATOR = ":"
